9d43bb508a1cdc87a9ba8c1b3a9a03d0fe3a628f
famafka <famafka@gmail.com>
2019-02-13 23:45:56 +0200

    minor bug fixed

--- AniRPG.Domain/Entities/Location.cs
+++ AniRPG.Domain/Entities/Location.cs
@@ -10,6 +10,11 @@ namespace AniRPG.Domain.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public ICollection<Location> Transitions { get; private set;}
+        public ICollection<Location> Transitions { get; private set; }
+
+        Location ()
+        {
+            Transitions = new HashSet<Location>();
+        }
     }
 }

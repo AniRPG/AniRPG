b6abb8d50b5a11cda11791671d185a295d211542
famafka <famafka@gmail.com>
2019-02-13 23:11:09 +0200

    Added inerface IEntity Added entities Character and Location

---
+++ AniRPG.Domain/Entities/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AniRPG.Domain.Common;
+
+namespace AniRPG.Domain.Entities
+{
+    public class Location : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Location> Transitions { get; private set;}
+    }
+}

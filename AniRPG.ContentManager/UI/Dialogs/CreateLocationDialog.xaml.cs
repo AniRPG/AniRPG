dbf763102a3f6432e4bf7a2c2d586edc39bca79d
aangairbender <jetixranger@ya.ru>
2019-02-15 02:11:45 +0200

    Some UI elements added

---
+++ AniRPG.ContentManager/UI/Dialogs/CreateLocationDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AniRPG.ContentManager.UI.Dialogs
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateLocationDialog.xaml
+    /// </summary>
+    public partial class CreateLocationDialog : Window
+    {
+        public string LocationName => LocationNameTextBox.Text;
+
+        public CreateLocationDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void AcceptButtonClickedHandler(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
--- AniRPG.ContentManager/UI/UserControls/LocationEditor.xaml.cs
+++ AniRPG.ContentManager/UI/UserControls/LocationEditor.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AniRPG.ContentManager.UI.Windows.Main
+namespace AniRPG.ContentManager.UI.UserControls
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для LocationEditor.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LocationEditor : UserControl
     {
-        public MainWindow()
+        public LocationEditor()
         {
             InitializeComponent();
         }
